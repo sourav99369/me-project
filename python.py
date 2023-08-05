135eacac7ada70f4b00c686c4a6dadf7cdff2317
sourav99369 <Souravdanga99369@gmail.com>
2023-08-05 12:58:42 +0530

    init

---
+++ python.py
@@ -0,0 +1,9 @@
+def add_three_numbers(num1, num2, num3):
+    return num1 + num2 + num3
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+result = add_three_numbers(num1, num2, num3)
+print("Sum of the three numbers:", result)
